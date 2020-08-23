23c293d5f3546af8f798efb3328ce855bfa9fded
Petrut Sergiu <petrutsergiu@yahoo.com>
2020-08-23 16:56:05 +0300

    initialise

---
+++ TeleHealth.User-Service/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using user_service.Models;
+using UserService.Data;
+
+namespace user_service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Testie")]
+        public User GetTestie()
+        {
+            MongoDAL mongo = new MongoDAL();
+            mongo.GetMongo();
+            return new User() { FirstName = "User-Sergiu", LastName = "Pete", Age = 30 };
+        }
+    }
+}
---
+++ UserService.Data/MongoDAL.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserService.Data
+{
+    public class MongoDAL
+    {
+        public void GetMongo()
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://SergiuUser:Password!123@cluster0.ct4no.azure.mongodb.net/Cluster0?retryWrites=true&w=majority");
+
+            var collection = dbClient.GetDatabase("User").GetCollection<BsonDocument>("User");
+            var filter = Builders<BsonDocument>.Filter.Eq("name", "Sergiu");
+            //var document = new BsonDocument { { "name", "Sergiu" }, { "Value", "great" } };
+            //collection.InsertOne(document);
+            //foreach (var db in dbList.fi)
+            //{
+            //    Console.WriteLine(db);
+            //}
+            var x = collection.Find(filter).FirstOrDefault().GetElement("_id");
+
+        }
+    }
+}
