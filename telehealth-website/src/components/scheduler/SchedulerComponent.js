bc72d50869047b03c06cc49d58b8b0733426924c
Petrut Sergiu <petrutsergiu@yahoo.com>
2021-02-08 14:25:18 +0200

    brickberg

--- telehealth-website/src/components/scheduler/SchedulerComponent.js
+++ telehealth-website/src/components/scheduler/SchedulerComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import Paper from '@material-ui/core/Paper';
 import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
 import {
@@ -10,26 +10,51 @@ import {
   AppointmentForm,
   AppointmentTooltip,
   ConfirmationDialog,
+  AllDayPanel,
+  Toolbar,
+  DateNavigator,
+  TodayButton,
 } from '@devexpress/dx-react-scheduler-material-ui';
 import request from '../../helpers/request';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 
 const SchedulerComponent = (props) => {
 
-  const appointments =
-  {
-    title: 'Website Re-Design Plan',
-    startDate: new Date(2018, 5, 25, 9, 35),
-    endDate: new Date(2018, 5, 25, 11, 30),
-    id: 0,
-    location: 'Room 1',
-  };
   const [data, setData] = useState([]);
+  const [currentDate, setCurrentDateChange] = useState(new Date());
+  const [currentViewName, setCurrentViewName] = useState();
+  const [isAppointmentBeingCreated, setIsAppointmentBeingCreated] = useState(false);
+
   const selectedDoctor = props;
   const doctorId = selectedDoctor.selectedDoctor.credentialsId;
 
-  const saveChanges = (data) => {
+  const ExternalViewSwitcher = ({
+    currentViewName,
+    onChange,
+  }) => (
+    <RadioGroup
+      aria-label="Views"
+      style={{ flexDirection: 'row' }}
+      name="views"
+      value={currentViewName}
+      onChange={onChange}
+    >
+      <FormControlLabel value="Week" control={<Radio />} label="Week" />
+      <FormControlLabel value="Month" control={<Radio />} label="Month" />
+    </RadioGroup>
+  );
 
+  const currentViewNameChange = (e) => {
+    setCurrentViewName(e.target.value);
+  }
+  const currentDateChange = (currentDate) => {
+    setCurrentDateChange(currentDate);
+  }
+
+  const saveChanges = (data) => {
     const appointments = data.map((ap) => ({
       appointmentId: ap.id,
       from: ap.startDate,
@@ -54,14 +79,45 @@ const SchedulerComponent = (props) => {
       endDate: ap.to,
       title: ap.title,
       allDay: ap.allDay,
-      notes: ap.notes
+      notes: ap.notes,
+      disabled: true
     }));
     return newData;
   }
 
+  const timeTableCell = ({ onDoubleClick, ...restProps }) => (
+    <WeekView.TimeTableCell
+      {...restProps}
+      onDoubleClick={!isAppointmentBeingCreated ? onDoubleClick : undefined}
+    />
+  );
+
+  
+  const onCommitChanges = ({ added, changed, deleted }) => {
+    if (added) {
+      const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+      setData([...data, { id: startingAddedId, ...added }]);
+      saveChanges([...data, { id: startingAddedId, ...added }]);
+      setIsAppointmentBeingCreated(true);
+    }
+    else if (changed) {
+      setData(data.map(appointment => (
+        changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment)));
+      saveChanges(data.map(appointment => (
+        changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment)));
+        setIsAppointmentBeingCreated(false);
+    }
+
+    else if (deleted !== undefined) {
+      setData(data.filter(appointment => appointment.id !== deleted));
+      saveChanges(data.filter(appointment => appointment.id !== deleted));
+      setIsAppointmentBeingCreated(false);
+    }
+  };
+
   useEffect(() => {
-    console.log('doftoru',doctorId);
-    let appointment = {doctorId};
+    console.log('doftoru', doctorId);
+    let appointment = { doctorId };
     request({
       url: `Appointments/GetAppointments`,
       method: 'post',
@@ -70,51 +126,51 @@ const SchedulerComponent = (props) => {
     }).then((res) => setData(covertToData(res.content)));
   }, [])
 
-
-  const commitChanges = ({ added, changed, deleted }) => {
-    if (added) {
-      console.log(added);
-      const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
-      setData([...data, { id: startingAddedId, ...added }]);
-      saveChanges([...data, { id: startingAddedId, ...added }]);
-    }
-    else if (changed) {
-      setData(data.map(appointment => (
-        changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment)));
-    }
-
-    else if (deleted !== undefined) {
-      setData(data.filter(appointment => appointment.id !== deleted));
-    }
-  }
-
-
-
   return (
-    <Paper>
-      {selectedDoctor.firstName}
-      <Scheduler
-        data={data}
-        height={660}
-      >
-        <ViewState
-          currentDate={new Date()}
-        />
-        <EditingState
-          onCommitChanges={commitChanges}
-        />
-        <IntegratedEditing />
-        {/* <WeekView startDayHour={9} endDayHour={19} /> */}
-        <MonthView/>
-        <ConfirmationDialog />
-        <Appointments />
-        <AppointmentTooltip
-          showOpenButton
-          showDeleteButton
-        />
-        <AppointmentForm />
-      </Scheduler>
-    </Paper>
+    <div>
+      <ExternalViewSwitcher
+        currentViewName={currentViewName}
+        onChange={currentViewNameChange}
+      />
+      <Paper>
+        {selectedDoctor.firstName}
+
+        <Scheduler
+          data={data}
+        >
+          <ViewState
+            defaultCurrentDate={currentDate}
+            currentViewName={currentViewName}
+            onCurrentDateChange={currentDateChange}
+          />
+          <EditingState
+            onCommitChanges={onCommitChanges}
+          />
+          <IntegratedEditing />
+          <WeekView
+            startDayHour={8}
+            endDayHour={19}
+            timeTableCellComponent={timeTableCell}
+          />
+          <MonthView />
+          <Appointments />
+          <ConfirmationDialog
+            ignoreCancel
+          />
+          <AppointmentTooltip
+            showOpenButton
+            showDeleteButton
+          />
+          <AppointmentForm
+          
+          />
+          <Toolbar />
+          <DateNavigator />
+          <TodayButton />
+          <AllDayPanel />
+        </Scheduler>
+      </Paper>
+    </div>
   );
 }
 
